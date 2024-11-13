db5d10a1d13b3fa450b6ff5ac198a6e53cbbc021
Moluno-xiii <molunoprogress@gmail.com>
2024-11-13 18:31:36 +0100

    initial commit

---
+++ src/App.tsx
@@ -0,0 +1,13 @@
+import "./App.css";
+import Hero from "./components/Hero";
+
+function App() {
+  return (
+    <div className="bg-pink-600 text-purple-400">
+      welcome
+      <Hero />
+    </div>
+  );
+}
+
+export default App;
---
+++ src/components/Hero.tsx
@@ -0,0 +1,7 @@
+export default function Hero() {
+  return (
+    <div className="text-orange-600">
+      <div className="text-4xl font-bold text-purple-400">noonecares</div>
+    </div>
+  );
+}
