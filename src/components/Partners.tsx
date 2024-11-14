5173770faa42475abc23dae49369766696f25522
Moluno-xiii <molunoprogress@gmail.com>
2024-11-14 16:15:32 +0100

    feat : added animations

--- src/components/Partners.tsx
+++ src/components/Partners.tsx
@@ -90,48 +90,6 @@ const Partners = () => {
 };
 
 export default Partners;
-// const Partners = () => {
-//   return (
-//     <section className="overflow-x-hidden">
-//       <div
-//         className="mx-auto flex max-w-7xl flex-col px-5 py-10 lg:px-10"
-//         id="partners"
-//       >
-//         <div className="flex flex-row">
-//           <div className="hidden md:w-[500px] lg:block">
-//             <img
-//               src="/partners-image.avif"
-//               alt="image of man and woman smiling"
-//               className="h-full w-full"
-//             />
-//           </div>
-//           <div className="flex flex-1 flex-col items-center gap-y-7 px-10 pb-10">
-//             <p className="text-2xl font-bold">
-//               We've stopped counting. Over 500 brands count on us.
-//             </p>
-//             <span className="text-lg font-semibold text-gray-400">
-//               1,200+ projects executed successfully and an average relationship
-//               of over 3 years.
-//             </span>
-//             <button className="border-b-2 border-black transition-all duration-100 hover:translate-x-5 hover:border-orange-500 hover:text-orange-500 lg:self-start">
-//               <span className="flex flex-row items-center justify-between gap-x-10 transition-all duration-300 hover:translate-x-5">
-//                 Our greatest hits <HiOutlineArrowLongRight />
-//               </span>
-//             </button>
-//           </div>
-//         </div>
-//         <ul className="mt-10 flex w-fit flex-row flex-nowrap gap-7  bg-transparent lg:-translate-y-60">
-//            <ul className="mt-10 grid grid-cols-1 gap-7 bg-transparent sm:grid-cols-2 md:grid-cols-3 lg:-translate-y-60">
-//           {partnersInfo.map((partner) => (
-//             <PartnerComponent partner={partner} key={partner.title} />
-//           ))}
-//         </ul>
-//       </div>
-//     </section>
-//   );
-// };
-
-// export default Partners;
 
 interface PartnerComponentProps {
   partner: {
@@ -159,23 +117,3 @@ const PartnerComponent: React.FC<PartnerComponentProps> = ({ partner }) => {
     </li>
   );
 };
-
-// const PartnerComponent: React.FC<PartnerComponentProps> = ({ partner }) => {
-//   return (
-//     <li
-//       key={partner.title}
-//       className="flex w-[300px] flex-col gap-y-7 rounded-xl bg-[#f5f5f5] px-4 py-6 shadow-md transition-all duration-300 hover:shadow-xl"
-//     >
-//       <div className="flex flex-row items-center justify-between">
-//         <img src={partner.imageUrl} className="h-5" alt="partner's image" />
-//         <HiArrowRight className="hover:text-orange-500" />
-//       </div>
-//       <p className="text-xs">"{partner.text}"</p>
-
-//       <div className="mt-auto flex flex-col text-sm">
-//         <span className="font-bold">{partner.author}</span>
-//         <span>{partner.job}</span>
-//       </div>
-//     </li>
-//   );
-// };
